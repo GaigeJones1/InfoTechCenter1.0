e402c4018a13c72842dfdbc49cbb9692f1bba763
Gaige Jones <Gaige.thomas.jones@gmail.com>
2024-10-21 08:34:56 -0400

    Added 2 Functions For Gas Level And Stations - Called - Stable

---
+++ CastleBravo.py
@@ -0,0 +1,17 @@
+print("\n**********************************************\n")
+
+print("Gasoline Branch")
+
+import random
+from time import sleep
+
+def gasLevelGauge():
+    gasLevelList = ["Empty", "Low", "Quarter Tank", "Half Tank", "Three Quarter Tank", "Full Tank"]
+    return random.choice(gasLevelList)
+
+def gasStations():
+    gasStationsList = ["VP", "shell", "Meijer", "Sams Club", "Marathon", "Mobile", "Speedway", "Circle K"]
+    return random.choice(gasStationsList)
+
+print (gasLevelGauge())
+print (gasStations())
